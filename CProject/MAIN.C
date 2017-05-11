0c362dd837e2dc0648797a0283ef8da0ba834a83
sususweet <tyq9702@sina.com>
2017-05-11 13:08:16 +0800

    Unit test passed.

--- CProject/MAIN.C
+++ CProject/MAIN.C
@@ -1,9 +1,13 @@
 #include <reg51.h>
 #include <intrins.h>
+/*#include "REG51.H"
+#include "INTRINS.H"  */
 
 /*char code SST516[3] _at_ 0x003b;*/
 
 #define DIGITAL_ARR_NUM 18
+#define TWINKLE_ARR_NUM 8
+
 /*定时器0 0.1ms*/
 #define TIMER0_TH 0x9C
 #define TIMER0_TL 0x9C
@@ -12,47 +16,52 @@
 #define DLE P3        // 段选
 
 void led_disp_bit(unsigned int led_data_bit, unsigned int i);
-
 void led_twinkle(unsigned int led_data[]);
-
 void setTime();
 void scan_key();
 void time_inc();
+void init_timer0();
+void time_ms_clr();
+void time_ms_inc();
+/*void init_timer1();*/
 
 unsigned int press_key();
 unsigned int read_key();
-
 unsigned int is_leap_year(unsigned int year);
 
 // 数码管显示数据表
 unsigned char digital[DIGITAL_ARR_NUM] =
         {0x28, 0x7E, 0xA2, 0x62, 0x74, 0x61, 0x21, 0x7A, 0x20,
          0x60, 0x30, 0x25, 0xA9, 0x26, 0xA1, 0xB1, 0xFF, 0xF7};
-unsigned int key_map[4][4] =
-        {{0, 1, 2, 3},{4, 5, 6, 7},{8, 9, 10, 11},{12, 13, 14, 15}};
-enum key_states_e{
+unsigned int twinkle_bit[TWINKLE_ARR_NUM] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+enum key_states_e {
     KEY_STATE_RELEASE,
     KEY_STATE_WAITING,
     KEY_STATE_PRESSED
 };
-enum show_mode{
-    TIME,
-    DATE,
-    COUNT
+enum show_mode {
+    TIME, DATE, COUNT
+};
+enum timer_mode {
+    CLEAR, STOP, START
+};
+enum setting_mode {
+    NONE, SECOND, MINUTE, HOUR, DAY, MONTH, YEAR
 };
 
 unsigned int sec = 0, minute = 0, hour = 12;
 unsigned int day = 1, month = 1, year = 2017;
+unsigned int count_msec = 0, count_sec = 0, count_minute = 0;
 unsigned int disp_data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned int show_stage = TIME;
+unsigned int timer_stage = CLEAR;
+unsigned int setting_stage = NONE;
 
 int main(void) {
-    TMOD = 0x02;
-    ET0 = 1;
+    TMOD = 0x22;
     EA = 1;
-    TR0 = 1;
-    TH0 = TIMER0_TH;
-    TL0 = TIMER0_TL;
+    init_timer0();
     setTime();
     while (1) {}
     return 0;
@@ -64,102 +73,264 @@ void interrupt0() {
     return;
 }
 
-void opr_key(unsigned int key_code){
-    switch(key_code){
+void init_timer0() {
+    ET0 = 1;
+    TR0 = 1;
+    TH0 = TIMER0_TH;
+    TL0 = TIMER0_TL;
+}
+
+void clr_twinkle(){
+    unsigned int i;
+    for (i = 0; i < TWINKLE_ARR_NUM; i++){
+        twinkle_bit[i] = 0;
+    }
+    return;
+}
+/*void init_timer1(){
+    ET1 = 1;
+    TR1 = 1;
+    TH1 = TIMER1_TH;
+    TL1 = TIMER1_TL;
+}*/
+
+void opr_key(unsigned int key_code) {
+    switch (key_code) {
         case 0:
-            switch (show_stage){
-                case TIME:
+            switch (show_stage) {
+                case TIME:{
                     show_stage = DATE;
+                    setting_stage = NONE;
+                    clr_twinkle();
                     break;
-                case DATE:
+                }
+                case DATE:{
+                    show_stage = COUNT;
+                    setting_stage = NONE;
+                    clr_twinkle();
+                    break;
+                }
+                case COUNT: {
                     show_stage = TIME;
+                    setting_stage = NONE;
+                    clr_twinkle();
+                    break;
+                }
+                default:
                     break;
             }
             break;
         case 1:
+            if (show_stage == COUNT) {
+                switch (timer_stage) {
+                    case START: {
+                        timer_stage = STOP;
+                        break;
+                    }
+                    case STOP:{
+                        timer_stage = CLEAR;
+                        time_ms_clr();
+                        break;
+                    }
+                    case CLEAR:{
+                        timer_stage = START;
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }else {
+                switch (setting_stage) {
+                    case SECOND:
+
+                        break;
+                    case MINUTE:
+
+                        break;
+                    case HOUR:
+
+                        break;
+                    case DAY:
+
+                        break;
+                    case MONTH:
+
+                        break;
+                    case YEAR:
+
+                        break;
+                    default:break;
+                }
+            }
+            break;
+        case 2:
+            if (show_stage == TIME) {
+                switch (setting_stage) {
+                    case NONE:{
+                        setting_stage = SECOND;
+                        break;
+                    }
+
+                    case SECOND:{
+                        setting_stage = MINUTE;
+                        break;
+                    }
+
+                    case MINUTE:{
+                        setting_stage = HOUR;
+                        break;
+                    }
+
+                    case HOUR:{
+                        setting_stage = NONE;
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }else if (show_stage == DATE) {
+                switch (setting_stage) {
+                    case NONE:{
+                        setting_stage = DAY;
+                        break;
+                    }
+                    case DAY:{
+                        setting_stage = MONTH;
+                        break;
+                    }
+                    case MONTH: {
+                        setting_stage = YEAR;
+                        break;
+                    }
+                    case YEAR:{
+                        setting_stage = NONE;
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
             break;
         default:
             break;
     }
 }
 
-void scan_key(){
+void scan_key() {
     static key_state = KEY_STATE_RELEASE;
     unsigned int wait_time = 0;
     static scan_time = 0;
-    switch(key_state) {
-        case KEY_STATE_RELEASE:{
-            if (press_key() == 1){
+    switch (key_state) {
+        case KEY_STATE_RELEASE: {
+            if (press_key() == 1) {
                 key_state = KEY_STATE_WAITING;
             }
             break;
         }
-        case KEY_STATE_WAITING:{
-            if (press_key() == 1){
+        case KEY_STATE_WAITING: {
+            if (press_key() == 1) {
                 scan_time++;
-                if (scan_time >= wait_time){
+                if (scan_time >= wait_time) {
                     key_state = KEY_STATE_PRESSED;
                     scan_time = 0;
                 }
-            }else {
+            } else {
                 scan_time = 0;
                 key_state = KEY_STATE_RELEASE;
             }
             break;
         }
-        case KEY_STATE_PRESSED:{
+        case KEY_STATE_PRESSED: {
             unsigned key_code = read_key();
-            if (press_key() == 0){
+            if (press_key() == 0) {
                 opr_key(key_code);
                 key_state = KEY_STATE_RELEASE;
             }
             break;
         }
+        default:
+            break;
     }
 }
+
 //判断是否有键按下
-unsigned int press_key(){
-    unsigned char temp_line,temp_row;
+unsigned int press_key() {
+    unsigned char temp_line, temp_row;
     KEYBOARD = 0xF0;
     temp_line = KEYBOARD;
     if (temp_line == 0xF0) return 0;
+
     KEYBOARD = 0x0F;
     temp_row = KEYBOARD;
-    if (temp_line == 0x0F) return 0;
+    if (temp_row == 0x0F) return 0;
     return 1;
 }
 
 //扫描键盘，返回键值
-unsigned int read_key(){
-    unsigned char temp_column,temp_row,key_code;
-    unsigned int row,column;
+unsigned int read_key() {
+    unsigned char temp_column, temp_row;
+    unsigned int row, column, key_code;
     KEYBOARD = 0xF0;
     temp_column = KEYBOARD;
 
     KEYBOARD = 0x0F;
     temp_row = KEYBOARD;
 
-    switch (temp_column){
-        case 0x70: column = 0; break;
-        case 0xB0: column = 1; break;
-        case 0xD0: column = 2; break;
-        case 0xE0: column = 3; break;
+    switch (temp_column) {
+        case 0x70:{
+            column = 0;
+            break;
+        }
+
+        case 0xB0:{
+            column = 1;
+            break;
+        }
+
+        case 0xD0:{
+            column = 2;
+            break;
+        }
+
+        case 0xE0:{
+            column = 3;
+            break;
+        }
+
+        default:
+            break;
     }
 
-    switch (temp_row){
-        case 0x07: row = 0; break;
-        case 0x0B: row = 1; break;
-        case 0x0D: row = 2; break;
-        case 0x0E: row = 3; break;
+    switch (temp_row) {
+        case 0x07: {
+            row = 0;
+            break;
+        }
+        case 0x0B:{
+            row = 1;
+            break;
+        }
+        case 0x0D:{
+            row = 2;
+            break;
+        }
+        case 0x0E:{
+            row = 3;
+            break;
+        }
+        default:
+            break;
     }
-    key_code = key_map[row][column];
+    key_code = 4 * row + column;
     return key_code;
 }
 
 
 void setTime() {
-    switch (show_stage){
-        case TIME:{
+    switch (show_stage) {
+        case TIME: {
+            twinkle_bit[2] = 1;
+            twinkle_bit[5] = 1;
             disp_data[0] = hour / 10;
             disp_data[1] = hour % 10;
             disp_data[2] = 17;
@@ -170,16 +341,32 @@ void setTime() {
             disp_data[7] = sec % 10;
             break;
         }
-        case DATE:{
+        case DATE: {
             disp_data[0] = year / 1000;
-            disp_data[1] = year % 1000 /100;
+            disp_data[1] = year % 1000 / 100;
             disp_data[2] = year % 1000 % 100 / 10;
             disp_data[3] = year % 1000 % 100 % 10;
             disp_data[4] = month / 10;
             disp_data[5] = month % 10;
             disp_data[6] = day / 10;
             disp_data[7] = day % 10;
+            break;
         }
+        case COUNT: {
+            twinkle_bit[2] = 1;
+            twinkle_bit[5] = 1;
+            disp_data[0] = count_minute / 10;
+            disp_data[1] = count_minute % 10;
+            disp_data[2] = 17;
+            disp_data[3] = count_sec / 10;
+            disp_data[4] = count_sec % 10;
+            disp_data[5] = 17;
+            disp_data[6] = count_msec / 10;
+            disp_data[7] = count_msec % 10;
+            break;
+        }
+        default:
+            break;
     }
 }
 
@@ -195,15 +382,24 @@ void led_twinkle(unsigned int led_data[]) {
     static unsigned int time = 0, flag = 0, i = 0;
     WLE = 0xFF;
     DLE = digital[16];
-    if (led_data[i] != 17) {
-        led_disp_bit(led_data[i], i);
-    } else {
+
+    if (twinkle_bit[i] == 1){
         if (flag == 0) {
-            led_disp_bit(17, i);
+            led_disp_bit(led_data[i], i);
         } else {
             led_disp_bit(16, i);
         }
+    }else{
+        led_disp_bit(led_data[i], i);
     }
+
+    /*if (led_data[i] != 17) {
+        led_disp_bit(led_data[i], i);
+    } else {
+
+    }*/
+
+
     i++;
     if (i >= 8) i = 0;
     time++;
@@ -226,48 +422,83 @@ void time_inc() {
     }
     if (hour >= 24) {
         hour = 0;
-        if(month == 1|month == 3|month==5|month==7|month==8|month==10|month==12) {
+        if (month == 1 | month == 3 | month == 5 | month == 7 | month == 8 | month == 10 | month == 12) {
             day++;
-            if(day>31){
-                day= 1 ;month++;
-                if(month==13){
-                    month=1;year++;
+            if (day > 31) {
+                day = 1;
+                month++;
+                if (month == 13) {
+                    month = 1;
+                    year++;
                 }
             }
         }
-        if(month==4|month==6|month==9|month==11) {
+        if (month == 4 | month == 6 | month == 9 | month == 11) {
             day++;
-            if(day>30){
-                day=1;month++;
+            if (day > 30) {
+                day = 1;
+                month++;
             }
         }
-        if(month==2) {
+        if (month == 2) {
             day++;
-            if(is_leap_year(year)){
-                if(day>=30)day=1;
-            }
-            else {
-                if(day>=29)day=1;
+            if (is_leap_year(year)) {
+                if (day >= 30)day = 1;
+            } else {
+                if (day >= 29)day = 1;
             }
         }
     }
     setTime();
     return;
 }
-unsigned int is_leap_year(unsigned int year){
+
+void time_ms_clr() {
+    count_msec = 0;
+    count_sec = 0;
+    count_minute = 0;
+    setTime();
+    return;
+}
+
+void time_ms_inc() {
+    count_msec++;
+    if (count_msec >= 100) {
+        count_sec += 1;
+        count_msec = 0;
+    }
+    if (count_sec >= 60) {
+        count_minute += 1;
+        count_sec = 0;
+    }
+    if (count_minute >= 60) {
+        count_sec = 0;
+        count_msec = 0;
+        count_minute = 0;
+    }
+    setTime();
+    return;
+}
+
+unsigned int is_leap_year(unsigned int year) {
     unsigned int leap_year;
-    if((year+2000)%400==0) leap_year=1;    //  被400整除为闰年
-    else if((year+2000)%100==0) leap_year=0;  // 不能被400整除 能被100整除 不是闰年
-    else if((year+2000)%4==0) leap_year=1;  // 不能被400、100整除 能被4整除 是闰年
-    else leap_year=0;
+    if ((year + 2000) % 400 == 0) leap_year = 1;    //  被400整除为闰年
+    else if ((year + 2000) % 100 == 0) leap_year = 0;  // 不能被400整除 能被100整除 不是闰年
+    else if ((year + 2000) % 4 == 0) leap_year = 1;  // 不能被400、100整除 能被4整除 是闰年
+    else leap_year = 0;
     return leap_year;
 }
-void int0() interrupt 1{
-    static unsigned int timer_num = 0;
-    timer_num++;
 
-    if (timer_num >= 5000){
+void int0() interrupt 1{
+    static unsigned int time_num = 0, timer_num = 0;
+    time_num++;
+    timer_num++;
+    if (timer_stage == START && timer_num >= 50) {
         timer_num = 0;
+        time_ms_inc();
+    }
+    if (time_num >= 5000) {
+        time_num = 0;
         time_inc();
         //TH0 = TIMER0_TH;
         //TL0 = TIMER0_TL;
